0bd701065f58ef6e18b404f8386ea51598fa84c3
Egor Smetanin <egorsmetanin2006@gmail.com>
2025-02-14 09:46:14 +0400

    Add functional targets

--- js/components/Target/Targets.js
+++ js/components/Target/Targets.js
@@ -1,3 +1,19 @@
 class Targets extends Component {
 
+    
+    targetShotHandler() {
+        const count = document.querySelector('.target-input').value - 0;
+        if (count > 0) {
+            const result = (new Shot()).shotToTarget(count);    
+            document.querySelector('.target-result').innerHTML=`Результат: ${result}`;
+        }
+    }
+    
+    
+
+
+    addEventListeners() {
+        document.querySelector('.target-shot')
+            .addEventListener('click', () => this.targetShotHandler());
+    }
 }
--- js/components/Target/targetsTemplate.js
+++ js/components/Target/targetsTemplate.js
@@ -1,2 +1,9 @@
-Template.prototype.targetsTemplate =()=>
-`Разметка про мишени`
+Template.prototype.targetsTemplate =()=>`
+<h2>Мишени</h2>
+<div>
+    <span>Сколько выстрелов делаем?</span><br>
+    <input class="target-input">
+</div>
+<button class="target-shot">Выстрел!</button>
+<span class="target-result">Результат: </span>
+`
