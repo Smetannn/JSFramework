158d8b993af64ce165b9ee4cf856e07d7e50d92e
Egor Smetanin <egorsmetanin2006@gmail.com>
2025-03-28 08:11:45 +0400

    Bringing it into one style

--- js/modules/math3D/figures/HyperbolicCylinder.js
+++ js/modules/math3D/figures/HyperbolicCylinder.js
@@ -1,6 +1,9 @@
 class HyperbolicCylinder extends Figure {
     constructor(count = 10){
         super();
+        const points = [];
+        const edges = [];
+        const polygons = [];
         
 
         //points
@@ -10,7 +13,7 @@ class HyperbolicCylinder extends Figure {
                 const x = i + size / count;
                 const y = x * x / size;
                 const z = j - size;
-                this.points.push(new Point(x,y,z));
+                points.push(new Point(x,y,z));
             }
         }
 
@@ -20,49 +23,49 @@ class HyperbolicCylinder extends Figure {
                 const x = i - size / count;
                 const y = x * x / size;
                 const z = j + size;
-                this.points.push(new Point(x,y,z));
+                points.push(new Point(x,y,z));
             }
         }
         //edges
-        for(let i = 0; i < this.points.length / 2; i++){
-            if (i + 1 < this.points.length / 2 && (i + 1) % count !== 0){
-                this.edges.push(new Edge(i, i + 1));
+        for(let i = 0; i < points.length / 2; i++){
+            if (i + 1 < points.length / 2 && (i + 1) % count !== 0){
+                edges.push(new Edge(i, i + 1));
             } else if ((i + 1) % count === 0){
-                this.edges.push(new Edge(i, i + 1 - count));
+                edges.push(new Edge(i, i + 1 - count));
             }
-            if (i < this.points.length / 2 - count){
-                this.edges.push(new Edge(i, i + count));
+            if (i < points.length / 2 - count){
+                edges.push(new Edge(i, i + count));
             }
         }
 
-        for (let i = this.points.length / 2; i < this.points.length; i++) {
-            if (i + 1 < this.points.length && (i + 1) % count !== 0) {
-                this.edges.push(new Edge(i, i + 1));
+        for (let i = points.length / 2; i < points.length; i++) {
+            if (i + 1 < points.length && (i + 1) % count !== 0) {
+                edges.push(new Edge(i, i + 1));
             } else if ((i + 1) % count === 0) {
-                this.edges.push(new Edge(i, i - count + 1));
+                edges.push(new Edge(i, i - count + 1));
             }
-            if (i < this.points.length - count) {
-                this.edges.push(new Edge(i, i + count));
+            if (i < points.length - count) {
+                edges.push(new Edge(i, i + count));
             }
         }
         //polygons
-    for (let i = 0; i < this.points.length / 2 - count; i++) {
-        if (i + 1 + count < this.points.length && (i + 1) % count !== 0) {
-            this.polygons.push(new Polygon([i, i + 1, i + 1 + count, i + count]));
-        } else if (i + count < this.points.length && (i + 1) % count === 0) {
-            this.polygons.push(new Polygon([i, i + 1 - count, i + 1, i + count]));
+    for (let i = 0; i < points.length / 2 - count; i++) {
+        if (i + 1 + count < points.length && (i + 1) % count !== 0) {
+            polygons.push(new Polygon([i, i + 1, i + 1 + count, i + count]));
+        } else if (i + count < points.length && (i + 1) % count === 0) {
+            polygons.push(new Polygon([i, i + 1 - count, i + 1, i + count]));
         }
     }
-    for (let i = Math.floor(this.points.length / 2); i < this.points.length - count; i++) {
-        if (i + 1 + count < this.points.length && (i + 1) % count !== 0) {
-            this.polygons.push(new Polygon([i, i + 1, i + 1 + count, i + count]));
-        } else if (i + count < this.points.length && (i + 1) % count === 0) {
-            this.polygons.push(new Polygon([i, i - count + 1, i + 1, i + count]));
+    for (let i = Math.floor(points.length / 2); i < points.length - count; i++) {
+        if (i + 1 + count < points.length && (i + 1) % count !== 0) {
+            polygons.push(new Polygon([i, i + 1, i + 1 + count, i + count]));
+        } else if (i + count < points.length && (i + 1) % count === 0) {
+            polygons.push(new Polygon([i, i - count + 1, i + 1, i + count]));
         }
     }
         
         
-        
+        return new Figure(points,edges,polygons);
 
 
     }
